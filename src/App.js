02712bb968e2bd0681739be266fee2a7530bd5e3
Nurbek <abduhamidovnurbek888@gmail.com>
2022-09-12 18:33:43 +0500

    Men uyga ketdim

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
     <div>
       <h1>
-        hello world
-       <h3>hi</h3>
-       
+        hello world Goodbye world
+        <h3>hi</h3>
       </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

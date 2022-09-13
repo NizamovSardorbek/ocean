b2229f59221dbabec26dd746679b7fe45d572b91
SardorbekDevelop <sardornizamov717@gmail.com>
2022-09-13 11:54:01 +0500

    messengers qoshdm

---
+++ src/components/contactstyle.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+  display: flex;
+  height: 420px;
+  margin: 40px 100px;
+`;
+export const ContactLeft = styled.div`
+  flex: 1;
+  height: 270px;
+`;
+export const LeftImages = styled.img`
+  height: 26%;
+`;
+export const Words = styled.h2`
+  font-size: 25px;
+`;
+export const LittleWords = styled.h6`
+  font-size: 15px;
+  line-height: 0px;
+`;

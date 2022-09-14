80e1763df4671f31da326cde9e3e79815cd1a532
SardorbekDevelop <sardornizamov717@gmail.com>
2022-09-14 10:36:41 +0500

    carusel ham tugadi

--- src/components/carusel.jsx
+++ src/components/carusel.jsx
@@ -1,39 +1,111 @@
-import React from "react";
-import Slider from "react-slick";
+import React, { useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { CaruselDiv } from "./caruselstyle";
+import Slider from "react-slick";
+import Users from "./assets/boy.png";
+import User2 from "./assets/girlboy.png";
+import User3 from "./assets/badboyu.png";
+import { Blue, Card, Card2, Container, Recent, User } from "./caruselstyle";
 
-function Corusel() {
+function Carusels() {
+  const mock = [
+    {
+      id: 1,
+      name: "Nurbek",
+      paragraph:
+        " I recently concluded a complete messaging revamp for an exciting com",
+      img: Users,
+    },
+    {
+      id: 2,
+      name: "Nurbek",
+      paragraph:
+        " I recently concluded a complete messaging revamp for an exciting com",
+      img: User3,
+    },
+    {
+      id: 3,
+      name: "Nurbek",
+      paragraph:
+        " I recently concluded a complete messaging revamp for an exciting com",
+      img: User2,
+    },
+    {
+      id: 4,
+      name: "Nurbek",
+      paragraph:
+        " I recently concluded a complete messaging revamp for an exciting com",
+      img: User3,
+    },
+  ];
+
+  const [data, setData] = useState(mock);
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{
+          ...style,
+          display: "block",
+          background: "black",
+          borderRadius: "14px",
+        }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{
+          ...style,
+          display: "block",
+          background: "#847d7d",
+          borderRadius: "14px",
+        }}
+        onClick={onClick}
+      />
+    );
+  }
+
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
 
   return (
-    <div>
-      <h1>single item</h1>
-      <CaruselDiv>
+    <>
+      <Container>
+        <h1> What Our Customers are Saying</h1>
         <Slider {...settings}>
-          <div>
-            <h3>11</h3>
-          </div>
-          <div>
-            <h3>22</h3>
-          </div>
-          <div>
-            <h3>33</h3>
-          </div>
-          <div>
-            <h3>44</h3>
-          </div>
+          {data.map((value) => {
+            return (
+              <Card>
+                <Card2>
+                  <Blue></Blue>
+                  <Recent>
+                    <h4>{value.paragraph}</h4>
+                    <h4>-- Leo Aminoff</h4>
+                    <h4>Product Designer</h4>
+                    <User src={value.img} />
+                  </Recent>
+                </Card2>
+              </Card>
+            );
+          })}
         </Slider>
-      </CaruselDiv>
-    </div>
+      </Container>
+    </>
   );
 }
 
-export default Corusel;
+export default Carusels;
